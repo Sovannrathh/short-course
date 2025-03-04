52c94862ba2fee5ff76fe2f00fc3b692ebb9ecef
Chhim Sonita <chhimsonita@key-3.local>
2025-03-04 22:15:08 +0700

    c

---
+++ short-course-project/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('payment');
+// });
+
+// Route::get('/', function () {
+//     return view('qr');
+// });
+
+Route::get('/', function () {
+    return view('card');
+});
+
+// Route::get('/', function () {
+//     return view('success');
+// });
