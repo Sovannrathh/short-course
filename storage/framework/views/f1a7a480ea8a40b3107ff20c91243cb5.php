8f0ee5bae854d83ff0949696882f70825968b4bb
Chhim Sonita <chhimsonita@key-3.local>
2025-03-04 21:54:09 +0700

    info_card_successMessage

---
+++ storage/framework/views/f1a7a480ea8a40b3107ff20c91243cb5.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Centered Box</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-black flex justify-center items-center p-10">
+    <div class="bg-gray-200 text-black w-[80%] h-[90%]">
+        <div>
+            <h1 class="text-center text-xl font-bold">Payment Successful</h1>
+        </div>
+        <div class="m-10">
+            <img src="https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg" alt="">
+        </div>
+    </div>
+</body>
+
+</html><?php /**PATH /Users/chhimsonita/Desktop/year3/term2/capstone/testing_laravel/resources/views/success.blade.php ENDPATH**/ ?>
